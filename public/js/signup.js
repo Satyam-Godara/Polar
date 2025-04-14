09563f98ac10034343996183e74283a18ab0a584
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-04-14 12:09:40 +0530

    Update signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         
         try {
-            const response = await fetch('https://polar-backend-6jr5.onrender.com', {
+            const response = await fetch('https://polar-backend-6jr5.onrender.com/users/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         
         try {
-            const response = await fetch('https://polar-backend-6jr5.onrender.com', {
+            const response = await fetch('https://polar-backend-6jr5.onrender.com/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
