f966a25c2968fae057fb3b2a7b68fc7ff18f9a5f
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-04-14 11:10:59 +0530

    Update profile.js
    render removed

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,6 @@
   // import { fetchAndRenderDashboardCharts } from './dashboard.js';
 
-const API_URL = 'https://polar-backend-6jr5.onrender.com';
+const API_URL = 'http://localhost:3000/api';
 const USD_TO_INR_RATE = 1; // No conversion needed, all amounts in INR
 
 
