5c4cd2381ed4f9b1dc135989a245ce905410c6bb
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-04-14 14:39:54 +0530

    Update category.js

--- public/js/category.js
+++ public/js/category.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const API_URL = 'https://polar-backend-6jr5.onrender.com';
+    const API_URL = 'https://polar-backend-6jr5.onrender.com/api';
     const user = JSON.parse(localStorage.getItem('user')) || {};
     let incomeBarChart, assetPieChart, expenseBarChart, liabilityPieChart;
     let showingAssetTable = false;
